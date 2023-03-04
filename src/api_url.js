a1a9873afc33ad86f6158e14df24a8bae1c3d994
unknown <mprakhar35@gmail.com>
2023-03-04 18:35:19 +0530

    some changes

--- src/api_url.js
+++ src/api_url.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://wallet-5klca.ondigitalocean.app/api";
 export default BASE_URL;
 
 //https://wallet-5klca.ondigitalocean.app/api/
