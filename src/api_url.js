4b3a095860b6b663aaa2a41d9ecbec1517168f1e
unknown <mprakhar35@gmail.com>
2023-03-04 18:52:23 +0530

    some changes

--- src/api_url.js
+++ src/api_url.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://wallet-5klca.ondigitalocean.app/api";
+const BASE_URL = "https://sea-turtle-app-z7f8m.ondigitalocean.app/api";
 export default BASE_URL;
 
 //https://wallet-5klca.ondigitalocean.app/api/
